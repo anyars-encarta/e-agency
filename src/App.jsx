558f7482b26cc57901bc723a89adc7f60e484e7f
anyars-encarta <anyarsencarta@gmail.com>
2024-04-17 14:01:23 +0000

    Add feature page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
+import Feature from "./components/Feature";
 
 const Container = styled.div`
   height: 100vh;
@@ -20,11 +21,17 @@ const IntroShape = styled.div`
 
 const App = () => {
   return (
-    <Container>
-      <Navbar />
-      <Intro />
-      <IntroShape />
-    </Container>
+    <>
+      <Container>
+        <Navbar />
+        <Intro />
+        <IntroShape />
+      </Container>
+      
+      <Container>
+        <Feature />
+      </Container>
+    </>
   )
 };
 
