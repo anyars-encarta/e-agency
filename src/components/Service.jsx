860777b9603f103084555475d931f841b0f27050
anyars-encarta <anyarsencarta@gmail.com>
2024-04-18 08:34:47 +0000

    Use tenary operator to render image and video

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -16,14 +16,14 @@ const Left = styled.div`
 `;
 
 const Image = styled.img`
-    display: ${props => props.open && 'none'};
+    // display: ${props => props.open && 'none'};
     height: 100%;
     width: 80%;
     margin-left: 100px;
 `;
 
 const Video = styled.video`
-    display: ${props => !props.open && 'none'};
+    // display: ${props => !props.open && 'none'};
     height: 300px;
     position: absolute;
     top: 0;
@@ -83,16 +83,21 @@ const Service = () => {
     return (
         <Container>
             <Left>
-                <Image open={open} src={How} />
-                <Video open={open} autoPlay loop controls src="https://player.vimeo.com/external/449759244.sd.mp4?s=d5f3da46ddc17aa69a7de84f1e420610ebd2a391&profile_id=139&oauth2_token_id=57447761" />
+                {/* <Image open={open} src={How} />
+                <Video open={open} autoPlay loop controls src="https://player.vimeo.com/external/449759244.sd.mp4?s=d5f3da46ddc17aa69a7de84f1e420610ebd2a391&profile_id=139&oauth2_token_id=57447761" /> */}
+                {open ? (
+                    <Video autoPlay loop controls src="https://player.vimeo.com/external/449759244.sd.mp4?s=d5f3da46ddc17aa69a7de84f1e420610ebd2a391&profile_id=139&oauth2_token_id=57447761" />
+                ) : (
+                    <Image src={How} />
+                )}
             </Left>
             <Right>
                 <Wrapper>
                     <Title>Simple Process to Start</Title>
                     <Desc>
-                        We provide digital experience services to startups and small 
-                        businesses to looking for a partner of their digital media, design & 
-                        development, lead generation and communications requirements. We work 
+                        We provide digital experience services to startups and small
+                        businesses to looking for a partner of their digital media, design &
+                        development, lead generation and communications requirements. We work
                         with you, not for you. Although we have great resources.
                     </Desc>
 
